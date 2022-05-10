30e831fba7040305e8b2fcb650d3630c14561d87
DonaldChung-HK <lpchungaa@gmail.com>
2022-05-10 18:17:07 +0100

    edited poseidon example for lysozyme

--- Example/poseidon_new.py
+++ Example/poseidon_new.py
@@ -14,7 +14,7 @@ def main():
     traj_file = os.path.join(wd,"data/1AKI_prod.trr")
     # loading data into individual universe
     main = mda.Universe(topo_file, traj_file)
-    poseidon_object = Poseidon(container=main, start=1, end=20, water=('SOL',))
+    poseidon_object = Poseidon(container=main, start=1, end=20, water=('SOL',), excludedResnames=("CL",))
     solventData, soluteData = poseidon_object.run_analysis(verbose=True)
     print("------------------------------SolventData------------------------------")
     print(solventData)
