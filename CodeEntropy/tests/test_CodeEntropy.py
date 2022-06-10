266aea49d1c1d57781a42b9aeae60d2f9c6fa00c
DonaldChung-HK <lpchungaa@gmail.com>
2022-06-10 11:05:58 +0100

    updated to use pytest fixture

--- CodeEntropy/tests/test_CodeEntropy.py
+++ CodeEntropy/tests/test_CodeEntropy.py
@@ -6,18 +6,53 @@ import numpy as np
 import pandas as pd
 import pytest
 
-def test_CodeEntropy_whole_molecule():
-    """test for computing entropy at whole molecule level"""
+
+@pytest.fixture(scope='session')
+def data_dir():
     data_dir = os.path.dirname(os.path.abspath(__file__))
+    return data_dir
+@pytest.fixture(scope='session')
+def dataContainer_DNA(data_dir):
     tprfile = os.path.join(data_dir,"data/md_A4_dna.tpr")
     trrfile = os.path.join(data_dir,"data/md_A4_dna_xf.trr")
-    tScale = 1.0
-    fScale = 1.0
-    temper = 300.0 #K
     u = mda.Universe(tprfile, trrfile)
-    dataContainer = DC.DataContainer(u)
+    dataContainer_DNA = DC.DataContainer(u)
+    return dataContainer_DNA
+
+@pytest.fixture(scope='session')
+def dataContainer_protein(data_dir):
+    tprfile = os.path.join(data_dir,"data/1AKI_ws.tpr")
+    trrfile = os.path.join(data_dir,"data/1AKI_ws.trr")
+    u = mda.Universe(tprfile, trrfile)
+    dataContainer_protein = DC.DataContainer(u)
+    return dataContainer_protein
+
+@pytest.fixture(scope='session')
+def tScale():
+    tScale = 1.0
+    return tScale
+@pytest.fixture(scope='session')
+def fScale():
+    fScale = 1.0
+    return fScale
+@pytest.fixture(scope='session')
+def temper():
+    temper = 300.0 #K
+    return temper
+@pytest.fixture(scope='session')
+def axis_list():
+    axis_list = ["C5'", "C4'", "C3'"]
+    return axis_list
+@pytest.fixture(scope='session')
+def thread():
+    tScale = 2
+    return 2
+
+
+def test_CodeEntropy_whole_molecule(dataContainer_DNA, tScale, fScale, temper):
+    """test for computing entropy at whole molecule level"""
     wm_entropyFF, wm_entropyTT = EF.compute_entropy_whole_molecule_level(
-        arg_hostDataContainer = dataContainer,
+        arg_hostDataContainer = dataContainer_DNA,
         arg_outFile = None,
         arg_selector = "all", 
         arg_moutFile = None,
@@ -30,19 +65,10 @@ def test_CodeEntropy_whole_molecule():
     assert wm_entropyFF == pytest.approx(47.230063169278864)
     assert wm_entropyTT == pytest.approx(44.35619230374928)
 
-def test_CodeEntropy_res_level():
+def test_CodeEntropy_res_level(dataContainer_DNA, tScale, fScale, temper, axis_list):
     """test for computing entropy at residue level"""
-    data_dir = os.path.dirname(os.path.abspath(__file__))
-    tprfile = os.path.join(data_dir,"data/md_A4_dna.tpr")
-    trrfile = os.path.join(data_dir,"data/md_A4_dna_xf.trr")
-    tScale = 1.0
-    fScale = 1.0
-    temper = 300.0 #K
-    u = mda.Universe(tprfile, trrfile)
-    axis_list = ["C5'", "C4'", "C3'"]
-    dataContainer = DC.DataContainer(u)
     res_entropyFF, res_entropyTT = EF.compute_entropy_residue_level(
-        arg_hostDataContainer = dataContainer,
+        arg_hostDataContainer = dataContainer_DNA,
         arg_outFile = None,
         arg_selector = 'all', 
         arg_moutFile = None,
@@ -57,19 +83,10 @@ def test_CodeEntropy_res_level():
     assert res_entropyTT == pytest.approx(285.7213867860228)
 
 
-def test_CodeEntropy_united_atom_level():
+def test_CodeEntropy_united_atom_level(dataContainer_DNA, tScale, fScale, temper, axis_list, data_dir):
     """test for computing entropy at united atom level"""
-    data_dir = os.path.dirname(os.path.abspath(__file__))
-    tprfile = os.path.join(data_dir,"data/md_A4_dna.tpr")
-    trrfile = os.path.join(data_dir,"data/md_A4_dna_xf.trr")
-    tScale = 1.0
-    fScale = 1.0
-    temper = 300.0 #K
-    u = mda.Universe(tprfile, trrfile)
-    axis_list = ["C5'", "C4'", "C3'"]
-    dataContainer = DC.DataContainer(u)
     UA_entropyFF, UA_entropyTT, res_df = EF.compute_entropy_UA_level(
-        arg_hostDataContainer = dataContainer,
+        arg_hostDataContainer = dataContainer_DNA,
         arg_outFile = None,
         arg_selector = 'all', 
         arg_moutFile = None,
@@ -84,24 +101,14 @@ def test_CodeEntropy_united_atom_level():
     
     assert UA_entropyFF == pytest.approx(1439.501746003283)
     assert UA_entropyTT == pytest.approx(107.67610435497281)
-    data_dir = os.path.join(data_dir,"data/Atom_level_res_data.csv")
-    res_ref = pd.read_csv(data_dir, na_values="nan")
+    csv_dir = os.path.join(data_dir,"data/Atom_level_res_data.csv")
+    res_ref = pd.read_csv(csv_dir, na_values="nan")
     pd.testing.assert_frame_equal(res_ref, res_df, check_dtype=False)
 
-def test_CodeEntropy_united_atom_level_multiprocess():
+def test_CodeEntropy_united_atom_level_multiprocess(dataContainer_DNA, tScale, fScale, temper, axis_list, thread, data_dir):
     """test for computing entropy at united atom level with multiprocess"""
-    data_dir = os.path.dirname(os.path.abspath(__file__))
-    tprfile = os.path.join(data_dir,"data/md_A4_dna.tpr")
-    trrfile = os.path.join(data_dir,"data/md_A4_dna_xf.trr")
-    tScale = 1.0
-    fScale = 1.0
-    temper = 300.0 #K
-    u = mda.Universe(tprfile, trrfile)
-    thread = 2
-    axis_list = ["C5'", "C4'", "C3'"]
-    dataContainer = DC.DataContainer(u)
     UA_entropyFF, UA_entropyTT, res_df = EF.compute_entropy_UA_level_multiprocess(
-        arg_hostDataContainer = dataContainer,
+        arg_hostDataContainer = dataContainer_DNA,
         arg_outFile = None,
         arg_selector = 'all', 
         arg_moutFile = None,
@@ -116,34 +123,24 @@ def test_CodeEntropy_united_atom_level_multiprocess():
     )
     assert UA_entropyFF == pytest.approx(1439.5017460032827)
     assert UA_entropyTT == pytest.approx(107.67610435497281)
-    data_dir = os.path.join(data_dir,"data/Atom_level_res_data.csv")
-    res_ref = pd.read_csv(data_dir, na_values="nan")
+    csv_dir = os.path.join(data_dir,"data/Atom_level_res_data.csv")
+    res_ref = pd.read_csv(csv_dir, na_values="nan")
     pd.testing.assert_frame_equal(res_ref, res_df, check_dtype=False)
 
-def test_CodeEntropy_topo_method5():
+def test_CodeEntropy_topo_method5(dataContainer_protein):
     """test for computing topographical entroy using method 5 AEM method. AEM is random so it is impossible to make test accurately"""
-    data_dir = os.path.dirname(os.path.abspath(__file__))
-    tprfile = os.path.join(data_dir,"data/1AKI_ws.tpr")
-    trrfile = os.path.join(data_dir,"data/1AKI_ws.trr")
-    u = mda.Universe(tprfile, trrfile)
-    dataContainer = DC.DataContainer(u)
     result_entropyAEM = EF.compute_topographical_entropy_AEM(
-        arg_hostDataContainer = dataContainer, 
+        arg_hostDataContainer = dataContainer_protein, 
         arg_selector = "all",
         arg_outFile = None, 
         arg_verbose = 5
     )
     assert 220.0 < result_entropyAEM < 230.0
 
-def test_CodeEntropy_topo_method3():
+def test_CodeEntropy_topo_method3(dataContainer_DNA):
     """test for computing topographical entroy using method 3 Correlation density method"""
-    data_dir = os.path.dirname(os.path.abspath(__file__))
-    tprfile = os.path.join(data_dir,"data/md_A4_dna.tpr")
-    trrfile = os.path.join(data_dir,"data/md_A4_dna_xf.trr")
-    u = mda.Universe(tprfile, trrfile)
-    dataContainer = DC.DataContainer(u)
     result_entropy3 = EF.compute_topographical_entropy_method3(
-        arg_hostDataContainer = dataContainer, 
+        arg_hostDataContainer = dataContainer_DNA, 
         arg_selector = "all",
         arg_outFile = None, 
         arg_verbose = 5
@@ -152,15 +149,10 @@ def test_CodeEntropy_topo_method3():
     reference = 211.99999999999983
     assert reference == pytest.approx(result)
 
-def test_CodeEntropy_topo_method0_SC():
+def test_CodeEntropy_topo_method0_SC(dataContainer_protein):
     """test for computing topographical entroy using method 3 Correlation density method"""
-    data_dir = os.path.dirname(os.path.abspath(__file__))
-    tprfile = os.path.join(data_dir,"data/1AKI_ws.tpr")
-    trrfile = os.path.join(data_dir,"data/1AKI_ws.trr")
-    u = mda.Universe(tprfile, trrfile)
-    dataContainer = DC.DataContainer(u)
     result_entropy0_SC = EF.compute_topographical_entropy0_SC(
-        arg_hostDataContainer = dataContainer, 
+        arg_hostDataContainer = dataContainer_protein, 
         arg_selector = "all",
         arg_outFile = None, 
         arg_verbose = 5
@@ -169,15 +161,10 @@ def test_CodeEntropy_topo_method0_SC():
     reference = 621.2072742842519
     assert reference == pytest.approx(result_entropy0_SC)
 
-def test_CodeEntropy_topo_method0_BB():
+def test_CodeEntropy_topo_method0_BB(dataContainer_protein):
     """test for computing topographical entroy using method 3 Correlation density method"""
-    data_dir = os.path.dirname(os.path.abspath(__file__))
-    tprfile = os.path.join(data_dir,"data/1AKI_ws.tpr")
-    trrfile = os.path.join(data_dir,"data/1AKI_ws.trr")
-    u = mda.Universe(tprfile, trrfile)
-    dataContainer = DC.DataContainer(u)
     result_entropy0_BB = EF.compute_topographical_entropy0_BB(
-        arg_hostDataContainer = dataContainer, 
+        arg_hostDataContainer = dataContainer_protein, 
         arg_selector = "all",
         arg_outFile = None, 
         arg_verbose = 5
@@ -186,15 +173,10 @@ def test_CodeEntropy_topo_method0_BB():
     reference = 728.8710286898643
     assert reference == pytest.approx(result_entropy0_BB)
 
-def test_CodeEntropy_topo_method1_SC():
+def test_CodeEntropy_topo_method1_SC(dataContainer_protein):
     """test for computing topographical entroy using method 1 some degree of randomness so a range is used"""
-    data_dir = os.path.dirname(os.path.abspath(__file__))
-    tprfile = os.path.join(data_dir,"data/1AKI_ws.tpr")
-    trrfile = os.path.join(data_dir,"data/1AKI_ws.trr")
-    u = mda.Universe(tprfile, trrfile)
-    dataContainer = DC.DataContainer(u)
     result_entropy1_SC = EF.compute_topographical_entropy1_SC(
-        arg_hostDataContainer = dataContainer, 
+        arg_hostDataContainer = dataContainer_protein, 
         arg_selector = "all",
         arg_outFile = None, 
         arg_verbose = 5
@@ -203,15 +185,10 @@ def test_CodeEntropy_topo_method1_SC():
     reference = 390.587
     assert 388.0 <= result_entropy1_SC <= 395.0
 
-def test_CodeEntropy_topo_method1_BB():
+def test_CodeEntropy_topo_method1_BB(dataContainer_protein):
     """test for computing topographical entroy using method 1"""
-    data_dir = os.path.dirname(os.path.abspath(__file__))
-    tprfile = os.path.join(data_dir,"data/1AKI_ws.tpr")
-    trrfile = os.path.join(data_dir,"data/1AKI_ws.trr")
-    u = mda.Universe(tprfile, trrfile)
-    dataContainer = DC.DataContainer(u)
     result_entropy1_BB = EF.compute_topographical_entropy1_BB(
-        arg_hostDataContainer = dataContainer, 
+        arg_hostDataContainer = dataContainer_protein, 
         arg_selector = "all",
         arg_outFile = None, 
         arg_verbose = 5
--- CodeEntropy/tests/test_poseidon.py
+++ CodeEntropy/tests/test_poseidon.py
@@ -3,14 +3,87 @@ import MDAnalysis as mda
 import datetime as dt
 from CodeEntropy.ClassCollection.PoseidonClass import Poseidon
 import pandas as pd
+import pytest
 
-def test_poseidon_moleculeLevel():
-    start = dt.datetime.now()
+# def test_poseidon_moleculeLevel():
+#     start = dt.datetime.now()
+#     wd = os.path.dirname(os.path.abspath(__file__))
+#     topo_file = os.path.join(wd,"data/poseidon_example.prmtop")
+#     traj_file = os.path.join(wd,"data/poseidon_example.trr")
+#     u = mda.Universe(topo_file, traj_file)
+#     poseidon_object = Poseidon(container=u, start=2, end=12)
+#     result = poseidon_object.run_analysis(level_list = ['moleculeLevel'], verbose=False)
+#     data_dir_solute = os.path.join(wd,"data/soluteVariables10.0EE_moleculeLevel.csv")
+#     solute_ref = pd.read_csv(data_dir_solute, na_values="nan")
+#     pd.testing.assert_frame_equal(solute_ref, result["moleculeLevel"]["soluteData"], check_dtype=False)
+#     data_dir_solvent = os.path.join(wd,"data/solventVariables10.0EE_moleculeLevel.csv")
+#     solvent_ref = pd.read_csv(data_dir_solvent, na_values="nan")
+#     pd.testing.assert_frame_equal(solvent_ref, result["moleculeLevel"]["solventData"], check_dtype=False)
+    
+# def test_poseidon_residLevel_resname():
+#     start = dt.datetime.now()
+#     wd = os.path.dirname(os.path.abspath(__file__))
+#     topo_file = os.path.join(wd,"data/poseidon_example.prmtop")
+#     traj_file = os.path.join(wd,"data/poseidon_example.trr")
+#     u = mda.Universe(topo_file, traj_file)
+#     poseidon_object = Poseidon(container=u, start=2, end=12)
+#     result = poseidon_object.run_analysis(level_list = ['residLevel_resname'], verbose=False)
+#     data_dir_solute = os.path.join(wd,"data/soluteVariables10.0EE_residLevel_resname.csv")
+#     solute_ref = pd.read_csv(data_dir_solute, na_values="nan")
+#     pd.testing.assert_frame_equal(solute_ref, result["residLevel_resname"]["soluteData"], check_dtype=False)
+#     data_dir_solvent = os.path.join(wd,"data/solventVariables10.0EE_residLevel_resname.csv")
+#     solvent_ref = pd.read_csv(data_dir_solvent, na_values="nan")
+#     pd.testing.assert_frame_equal(solvent_ref, result["residLevel_resname"]["solventData"], check_dtype=False)
+#     data_dir_contact = os.path.join(wd,"data/resid_contact_matrix_residLevel_resname.csv")
+#     contact_ref = pd.read_csv(data_dir_contact, na_values="nan")
+#     pd.testing.assert_frame_equal(contact_ref, result["residLevel_resname"]["contactMatrix"], check_dtype=False)
+
+# def test_poseidon_atomLevel():
+#     start = dt.datetime.now()
+#     wd = os.path.dirname(os.path.abspath(__file__))
+#     topo_file = os.path.join(wd,"data/poseidon_example.prmtop")
+#     traj_file = os.path.join(wd,"data/poseidon_example.trr")
+#     u = mda.Universe(topo_file, traj_file)
+#     poseidon_object = Poseidon(container=u, start=2, end=12)
+#     result = poseidon_object.run_analysis(level_list = ['atomLevel'], verbose=False)
+#     data_dir_solute = os.path.join(wd,"data/soluteVariables10.0EE_atomLevel.csv")
+#     solute_ref = pd.read_csv(data_dir_solute, na_values="nan")
+#     pd.testing.assert_frame_equal(solute_ref, result["atomLevel"]["soluteData"], check_dtype=False)
+#     data_dir_solvent = os.path.join(wd,"data/solventVariables10.0EE_atomLevel.csv")
+#     solvent_ref = pd.read_csv(data_dir_solvent, na_values="nan")
+#     pd.testing.assert_frame_equal(solvent_ref, result["atomLevel"]["solventData"], check_dtype=False)
+#     data_dir_contact = os.path.join(wd,"data/resid_contact_matrix_atomLevel.csv")
+#     contact_ref = pd.read_csv(data_dir_contact, na_values="nan")
+#     pd.testing.assert_frame_equal(contact_ref, result["atomLevel"]["contactMatrix"], check_dtype=False)
+
+# def test_poseidon_soluteContacts():
+#     start = dt.datetime.now()
+#     wd = os.path.dirname(os.path.abspath(__file__))
+#     topo_file = os.path.join(wd,"data/poseidon_example.prmtop")
+#     traj_file = os.path.join(wd,"data/poseidon_example.trr")
+#     u = mda.Universe(topo_file, traj_file)
+#     poseidon_object = Poseidon(container=u, start=2, end=12)
+#     result = poseidon_object.run_analysis(level_list = ['soluteContacts'], verbose=False)
+#     data_dir_solute = os.path.join(wd,"data/soluteVariables10.0EE_soluteContacts.csv")
+#     solute_ref = pd.read_csv(data_dir_solute, na_values="nan")
+#     pd.testing.assert_frame_equal(solute_ref, result["soluteContacts"]["soluteData"], check_dtype=False)
+#     data_dir_solvent = os.path.join(wd,"data/solventVariables10.0EE_soluteContacts.csv")
+#     solvent_ref = pd.read_csv(data_dir_solvent, na_values="nan")
+#     pd.testing.assert_frame_equal(solvent_ref, result["soluteContacts"]["solventData"], check_dtype=False)
+@pytest.fixture(scope='session')
+def wd():
     wd = os.path.dirname(os.path.abspath(__file__))
+    return wd
+@pytest.fixture(scope='session')
+def poseidon_object(wd):
     topo_file = os.path.join(wd,"data/poseidon_example.prmtop")
     traj_file = os.path.join(wd,"data/poseidon_example.trr")
     u = mda.Universe(topo_file, traj_file)
     poseidon_object = Poseidon(container=u, start=2, end=12)
+    return poseidon_object
+
+
+def test_poseidon_moleculeLevel(poseidon_object, wd):
     result = poseidon_object.run_analysis(level_list = ['moleculeLevel'], verbose=False)
     data_dir_solute = os.path.join(wd,"data/soluteVariables10.0EE_moleculeLevel.csv")
     solute_ref = pd.read_csv(data_dir_solute, na_values="nan")
@@ -19,13 +92,7 @@ def test_poseidon_moleculeLevel():
     solvent_ref = pd.read_csv(data_dir_solvent, na_values="nan")
     pd.testing.assert_frame_equal(solvent_ref, result["moleculeLevel"]["solventData"], check_dtype=False)
     
-def test_poseidon_residLevel_resname():
-    start = dt.datetime.now()
-    wd = os.path.dirname(os.path.abspath(__file__))
-    topo_file = os.path.join(wd,"data/poseidon_example.prmtop")
-    traj_file = os.path.join(wd,"data/poseidon_example.trr")
-    u = mda.Universe(topo_file, traj_file)
-    poseidon_object = Poseidon(container=u, start=2, end=12)
+def test_poseidon_residLevel_resname(poseidon_object, wd):
     result = poseidon_object.run_analysis(level_list = ['residLevel_resname'], verbose=False)
     data_dir_solute = os.path.join(wd,"data/soluteVariables10.0EE_residLevel_resname.csv")
     solute_ref = pd.read_csv(data_dir_solute, na_values="nan")
@@ -37,13 +104,7 @@ def test_poseidon_residLevel_resname():
     contact_ref = pd.read_csv(data_dir_contact, na_values="nan")
     pd.testing.assert_frame_equal(contact_ref, result["residLevel_resname"]["contactMatrix"], check_dtype=False)
 
-def test_poseidon_atomLevel():
-    start = dt.datetime.now()
-    wd = os.path.dirname(os.path.abspath(__file__))
-    topo_file = os.path.join(wd,"data/poseidon_example.prmtop")
-    traj_file = os.path.join(wd,"data/poseidon_example.trr")
-    u = mda.Universe(topo_file, traj_file)
-    poseidon_object = Poseidon(container=u, start=2, end=12)
+def test_poseidon_atomLevel(poseidon_object, wd):
     result = poseidon_object.run_analysis(level_list = ['atomLevel'], verbose=False)
     data_dir_solute = os.path.join(wd,"data/soluteVariables10.0EE_atomLevel.csv")
     solute_ref = pd.read_csv(data_dir_solute, na_values="nan")
@@ -55,13 +116,7 @@ def test_poseidon_atomLevel():
     contact_ref = pd.read_csv(data_dir_contact, na_values="nan")
     pd.testing.assert_frame_equal(contact_ref, result["atomLevel"]["contactMatrix"], check_dtype=False)
 
-def test_poseidon_soluteContacts():
-    start = dt.datetime.now()
-    wd = os.path.dirname(os.path.abspath(__file__))
-    topo_file = os.path.join(wd,"data/poseidon_example.prmtop")
-    traj_file = os.path.join(wd,"data/poseidon_example.trr")
-    u = mda.Universe(topo_file, traj_file)
-    poseidon_object = Poseidon(container=u, start=2, end=12)
+def test_poseidon_soluteContacts(poseidon_object, wd):
     result = poseidon_object.run_analysis(level_list = ['soluteContacts'], verbose=False)
     data_dir_solute = os.path.join(wd,"data/soluteVariables10.0EE_soluteContacts.csv")
     solute_ref = pd.read_csv(data_dir_solute, na_values="nan")
